765462b3b924cffea83c844a58279ddd2b7a12c8
eliza <dmitrienkomy@gmail.com>
2018-09-18 18:49:29 +0700

    Add project files.

---
+++ MergingIterator/MergingIterator.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <iterator>
+#include <initializer_list>
+#include <vector>
+// Начиная с С++17 std::iterator - deprecated
+template <typename T>
+class MergingIterator
+{
+	
+public:
+	explicit MergingIterator(std::initializer_list<std::pair<T,T>> list)
+	{
+		T minObj;
+
+		
+		bool isMinObjDefined = false;
+		size_t minObjIndex  = 0;
+		size_t index = 0;
+		for (auto& i : list)
+		{
+			if (!isMinObjDefined)
+			{
+				minObj = i.first;
+				isMinObjDefined = true;
+			}
+			else
+			{
+				if (*minObj > *(i.first)  )
+				{
+					minObj = i.first;
+					minObjIndex = index;
+				}
+			}
+			_iterators.push_back(i);
+			index++;
+		}
+		_currentIterator = minObj;
+		_currentIteratorIndex = minObjIndex;
+	}
+
+	typename std::iterator_traits<T>::value_type operator* ()
+	{
+		return *_currentIterator;
+	}
+
+
+	typename std::iterator_traits<T>::value_type operator++ ()
+	{
+		std::cout << typeid(_currentIterator).name();
+		_currentIterator++;
+		T minObj;
+		bool isMinObjDefined = false;
+		size_t minObjIndex = 0;
+		size_t index = 0;
+		for (auto& i : _iterators)
+		{
+			if (!isMinObjDefined)
+			{
+				minObj = i.first;
+				isMinObjDefined = true;
+			}
+			else
+			{
+				if (*minObj > *(i.first))
+				{
+					minObj = i.first;
+					minObjIndex = index;
+				}
+			}
+			_iterators.push_back(i);
+			index++;
+		}
+		_currentIterator = minObj;
+		_currentIteratorIndex = minObjIndex;
+
+		return *_currentIterator;
+	}
+	
+
+private:
+		std::vector<std::pair<T,T>> _iterators;
+		T _currentIterator;
+		size_t _currentIteratorIndex;
+};
+namespace std {
+	template <typename T>
+	struct iterator_traits<MergingIterator<T>> {
+		using iterator_category =
+			std::forward_iterator_tag;
+		using value_type = T;
+		//using pointer = ...;
+		//using reference = ...;
+		//using difference_type = ...;
+	};
+}
---
+++ MergingIterator/Source.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include "MergingIterator.h"
+#include <initializer_list>
+#include <iostream>
+using namespace std;
+int main()
+{
+	vector<int> a{ 1,2,3 };
+	vector<int> b{ 1,2,3 };
+	vector<int> c{ 1,2,3 };
+	//initializer_list<std::pair< vector<int>::iterator, vector<int>::iterator> = 
+	MergingIterator < vector<int>::iterator> merIterator({ { a.begin(),a.end() }, { b.begin(),b.end() }, { c.begin(),c.end() } });
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		cout << *merIterator;
+		++merIterator;
+	}
+
+}
